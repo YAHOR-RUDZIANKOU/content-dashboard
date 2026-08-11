fffc14582879dc1856f31f5bc4675b2f5b6b9b1e
YAHOR RUDZIANKOU <rudenkov.egor04@mail.ru>
2026-08-11 21:01:35 +0300

    feat(ui): implement smooth text and loader animation inside login button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,20 +1,36 @@
 import classes from "./Button.module.css";
+import { motion, AnimatePresence } from "framer-motion";
+import Loader from "../../Loader/Loader";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary" | "danger" | "danger-filled";
   size?: "md" | "lg";
+  isLoading?: boolean;
 };
 
 const Button = ({
+  isLoading=false,
   children,
   className,
   variant = "secondary",
   size = "md",
   ...rest
-}:ButtonProps) => {
+}: ButtonProps) => {
   const combinedClasses = `${classes.btn} ${classes[variant]} ${classes[size]} ${className || ""}`;
   return (
     <button className={combinedClasses} {...rest}>
+      <AnimatePresence>
+        {isLoading  && (
+          <motion.div
+            initial={{ width: 0, opacity: 0, marginRight: 0 }}
+            animate={{ width: 18, opacity: 1, marginRight: 8 }}
+            exit={{ width: 0, opacity: 0, marginRight: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Loader />
+          </motion.div>
+        )}
+      </AnimatePresence>
       {children}
     </button>
   );
