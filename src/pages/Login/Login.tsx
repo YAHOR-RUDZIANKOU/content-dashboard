0467413caac6ca70ef15a51c6e3fb0e4e8c4b2d8
YAHOR RUDZIANKOU <rudenkov.egor04@mail.ru>
2026-08-12 18:36:21 +0300

    feat(login): add logo to login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,6 +6,7 @@ import { loginThunk } from "../../store/slices/authSlice";
 import { useAppDispatch, useAppSelector } from "../../store/index";
 import { motion, AnimatePresence } from "framer-motion";
 import { clearError } from "../../store/slices/authSlice";
+import Logo from "../../components/Logo/Logo";
 
 const Login = () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -35,59 +36,62 @@ const Login = () => {
   };
   return (
     <div className={classes.wrapper}>
-      <div className={classes.modal__wrapper}>
-        <h1>С возвращением</h1>
-        <div className={classes.modal__subtitle}>
-          Введите email - найдём вас в списке пользователей.
-        </div>
-        <div className={classes.static__wrapper}>
-          <div className={classes.static__text}>Email</div>
-          <Input value={"Sincere@april.biz"} disabled />
-          <div className={classes.static__subtext}>
-            Например: Sincere@april.biz
+      <div className={classes.authBlock }>
+        <Logo />
+        <div className={classes.modal__wrapper}>
+          <h1>С возвращением</h1>
+          <div className={classes.modal__subtitle}>
+            Введите email - найдём вас в списке пользователей.
           </div>
-        </div>
-        <form onSubmit={handleSubmit} className={classes.modal} noValidate>
-          <label className={classes.static__wrapper}>
+          <div className={classes.static__wrapper}>
             <div className={classes.static__text}>Email</div>
-            <Input
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              type="email"
-              placeholder="Введите свою почту ..."
-              required
-              onFocus={handleInputFocus}
-            />
-            {emailError && (
-              <div className={classes.email__error}>{emailError}</div>
-            )}
-          </label>
-          <Button
-            disabled={status === "loading"}
-            type="submit"
-            variant="primary"
-            size="lg"
-            isLoading={status === "loading"}
-          >
-            <span>Войти</span>
-          </Button>
-        </form>
-        <AnimatePresence>
-          {error && (
-            <motion.div
-              initial={{ height: 0, opacity: 0 }}
-              animate={{ height: "auto", opacity: 1 }}
-              exit={{ height: 0, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              style={{ overflow: "hidden" }}
+            <Input value={"Sincere@april.biz"} disabled />
+            <div className={classes.static__subtext}>
+              Например: Sincere@april.biz
+            </div>
+          </div>
+          <form onSubmit={handleSubmit} className={classes.modal} noValidate>
+            <label className={classes.static__wrapper}>
+              <div className={classes.static__text}>Email</div>
+              <Input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                type="email"
+                placeholder="Введите свою почту"
+                required
+                onFocus={handleInputFocus}
+              />
+              {emailError && (
+                <div className={classes.email__error}>{emailError}</div>
+              )}
+            </label>
+            <Button
+              disabled={status === "loading"}
+              type="submit"
+              variant="primary"
+              size="lg"
+              isLoading={status === "loading"}
             >
-              <div className={classes.error__wrapper}>
-                <div className={classes.error__icon}></div>
-                <div className={classes.error__text}>{error}</div>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
+              <span>Войти</span>
+            </Button>
+          </form>
+          <AnimatePresence>
+            {error && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                style={{ overflow: "hidden" }}
+              >
+                <div className={classes.error__wrapper}>
+                  <div className={classes.error__icon}></div>
+                  <div className={classes.error__text}>{error}</div>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
